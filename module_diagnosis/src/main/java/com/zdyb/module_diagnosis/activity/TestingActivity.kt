c84c88a2ff3675e66f90e78a7c4d160df958b993
kitty <867720279@qq.com>
2023-12-27 13:35:17 +0800

    军车 自检功能2.0

--- module_diagnosis/src/main/java/com/zdyb/module_diagnosis/activity/TestingActivity.kt
+++ module_diagnosis/src/main/java/com/zdyb/module_diagnosis/activity/TestingActivity.kt
@@ -8,6 +8,8 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
+import com.blankj.utilcode.util.ConvertUtils
+import com.blankj.utilcode.util.ThreadUtils
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.qmuiteam.qmui.kotlin.onClick
@@ -32,6 +34,8 @@ import java.io.File
 import java.util.concurrent.TimeUnit
 
 class TestingActivity :BaseActivity<ActivityTestingBinding,BaseViewModel>() {
+    private var dis1: Disposable? = null
+    private var dis2: Disposable? = null
 
     override fun initViewModel(): BaseViewModel {
         return BaseViewModel()
@@ -46,11 +50,12 @@ class TestingActivity :BaseActivity<ActivityTestingBinding,BaseViewModel>() {
         binding.recyclerView.adapter = mAdapter
 
         val list = mutableListOf<TestingBean>()
-        list.add(TestingBean("检测协议ISO 9141",0,false,"结果:正常"))
-        list.add(TestingBean("检测协议ISO 14230",0,false,"结果:正常"))
-        list.add(TestingBean("检测协议ISO 15031",0,false,"结果:正常"))
-        list.add(TestingBean("检测协议ISO 27145",0,false,"结果:正常"))
-        list.add(TestingBean("检测协议SAE J1939",0,false,"结果:正常"))
+        list.add(TestingBean("USB/MCU通讯测试",0,false,"结果:正常"))
+        list.add(TestingBean("总驱动测试",0,false,"结果:正常"))
+        list.add(TestingBean("CAN驱动设置测试",0,false,"结果:正常"))
+        list.add(TestingBean("K线驱动设置测试",0,false,"结果:正常"))
+        list.add(TestingBean("SAEJ1939/ISO15031/ISO27145/ISO14229/协议设置测试",0,false,"结果:正常"))
+        list.add(TestingBean("ISO9141/ISO14230/协议设置测试",0,false,"结果:正常"))
         mAdapter.setList(list)
     }
     private val mAdapter: BaseQuickAdapter<TestingBean, BaseViewHolder> =
@@ -100,13 +105,18 @@ class TestingActivity :BaseActivity<ActivityTestingBinding,BaseViewModel>() {
     override fun onResume() {
         super.onResume()
 
-        setState(0,false,2)
+//        setState(0,false,2)
+//        test()
 
-            if (test()){
-                ss(true)
-            }else{
-                ss(false)
+
+
+        dis2 = Observable.just("")
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io()).subscribe {
+                test2()
             }
+
+
     }
 
 
@@ -126,6 +136,92 @@ class TestingActivity :BaseActivity<ActivityTestingBinding,BaseViewModel>() {
             runOnUiThread{
                 mAdapter.notifyItemChanged(index)
             }
+        }
+    }
+
+
+    fun test2(){
+        if (!BaseApplication.usbConnect){
+            viewModel.showToast("请将诊断接头连接到平板电脑")
+            return
+        }
+
+        //USB通讯测试
+        setState(0,true,2)
+        ConnDevices.sendData(byteArrayOf(0xa5.toByte(), 0xa5.toByte(), 0x00, 0x02, 0xff.toByte(), 0x00, 0x55))
+        val result = ConnDevices.outTimeReadData(500)
+        val stringResult = ConvertUtils.bytes2HexString(result)
+        if ("A5A50002FF0155" == stringResult){
+            setState(0,true,3)
+        }else{
+            setState(0,false,3)
+        }
+        Thread.sleep(1000)
+
+        //总驱动测试
+        setState(1,true,2)
+        ConnDevices.sendData(byteArrayOf(0xa5.toByte(), 0xa5.toByte(), 0x00, 0x04, 0x00, 0xff.toByte(), 0xff.toByte(), 0x00, 0x55))
+        val result2 = ConnDevices.outTimeReadData(500)
+        val stringResult2 = ConvertUtils.bytes2HexString(result2)
+        if ("A5A5000500FFFF000155" == stringResult2){
+            setState(1,true,3)
+        }else{
+            setState(1,false,3)
+        }
+
+        Thread.sleep(1000)
+        //CAN驱动测试
+        setState(2,true,2)
+        ConnDevices.sendData(byteArrayOf(0xa5.toByte(), 0xa5.toByte(), 0x00, 0x04, 0x00, 0xff.toByte(), 0xff.toByte(), 0x01, 0x55))
+        val result3 = ConnDevices.outTimeReadData(500)
+        val stringResult3 = ConvertUtils.bytes2HexString(result3)
+        if ("A5A5000500FFFF01FF55" == stringResult3){
+            setState(2,true,3)
+        }else{
+            //倒数第二个字节是具体第几路失败
+            setState(2,false,3)
+        }
+        Thread.sleep(1000)
+        //K线驱动测试
+        setState(3,true,2)
+        ConnDevices.sendData(byteArrayOf(0xa5.toByte(), 0xa5.toByte(), 0x00, 0x04, 0x00, 0xff.toByte(), 0xff.toByte(), 0x02, 0x55))
+        val result4 = ConnDevices.outTimeReadData(500)
+        val stringResult4 = ConvertUtils.bytes2HexString(result4)
+        if ("A5A5000500FFFF020155" == stringResult4){
+            setState(3,true,3)
+        }else{
+            //倒数第二个字节是具体第几路失败
+            setState(3,false,3)
+        }
+
+        Thread.sleep(1000)
+        //SAEJ1939协议  和can是一样的
+        //ISO15031协议
+        //ISO27145协议
+        setState(4,true,2)
+        ConnDevices.sendData(byteArrayOf(0xa5.toByte(), 0xa5.toByte(), 0x00, 0x04, 0x00, 0xff.toByte(), 0xff.toByte(), 0x01, 0x55))
+        val result5 = ConnDevices.outTimeReadData(500)
+        val stringResult5 = ConvertUtils.bytes2HexString(result5)
+        if ("A5A5000500FFFF01FF55" == stringResult5){
+            setState(4,true,3)
+        }else{
+            //倒数第二个字节是具体第几路失败
+            setState(4,false,3)
+        }
+
+        Thread.sleep(1000)
+        //ISO9141协议  和K线是一样的
+        //ISO14230协议
+        //K线驱动测试
+        setState(5,true,2)
+        ConnDevices.sendData(byteArrayOf(0xa5.toByte(), 0xa5.toByte(), 0x00, 0x04, 0x00, 0xff.toByte(), 0xff.toByte(), 0x02, 0x55))
+        val result6 = ConnDevices.outTimeReadData(500)
+        val stringResult6 = ConvertUtils.bytes2HexString(result6)
+        if ("A5A5000500FFFF020155" == stringResult6){
+            setState(5,true,3)
+        }else{
+            //倒数第二个字节是具体第几路失败
+            setState(5,false,3)
         }
     }
 
@@ -154,7 +250,7 @@ class TestingActivity :BaseActivity<ActivityTestingBinding,BaseViewModel>() {
 
         return false
     }
-    private var dis1: Disposable? = null
+
 
 
     fun ss(b: Boolean){
@@ -194,16 +290,12 @@ class TestingActivity :BaseActivity<ActivityTestingBinding,BaseViewModel>() {
                     }
                 }
             },{it.printStackTrace()})
-//
-//        dis1 = Observable.just(0..3) { emitter ->
-//
-//            emitter.onError(Throwable("${PathManager.ecuReportPath()}文件不存在"))
-//            emitter.onNext(dataList)
-//        }
-//            .subscribeOn(Schedulers.io())
-//            .observeOn(AndroidSchedulers.mainThread())
-//            .subscribe({
-//
-//            },{it.printStackTrace()})
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        dis1?.dispose()
+        dis2?.dispose()
     }
 }
